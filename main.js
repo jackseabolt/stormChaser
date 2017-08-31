ca11e1cef54e70683f67e8e93da3a7defe681b30
Anthony Mengel <anthonymengel1232@gmail.com>
2017-08-31 15:20:53 -0400

    added effects to our results

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const STORE = {
     state: [],
     route: 'start',
     searchterm: null,
-    data: null 
+    data: null,
+    currentsetting: 'today' 
 }; 
 
 function handleFormSubmit(){
@@ -38,15 +39,47 @@ function createApiRequest(searchterm){
     $.getJSON(apiurl, search, callback);
 }
 
+function createHtml() {
+    let string = ''; 
+    if (STORE.currentsetting === 'today') {
+        string = createToday();
+    } else if (STORE.currentsetting === '3Day') {
+        string = create3Day();
+    } else if (STORE.currentsetting === '5Day') {
+        string = create5Day();
+    } else {console.log('big targeting problem')}
+    renderHtml(string); 
+}
+
+function renderHtml(string){
+    console.log(string); 
+    $('.js-results-container').html(string);
+}
+
+function createToday() {
+    return `
+    <div class='today-result'>
+        <p>The current temperture is ${Math.floor((STORE.data.list[0].main.temp) * 9/5 - 459.67)} &#176F</p>
+        <p>${STORE.data.list[1].weather[0].description}</p>
+        <p>There is currently ${STORE.data.list[0].main.humidity}% humidity</p>
+        <p>The wind is currently traveling at ${Math.floor((STORE.data.list[0].wind.speed)* 60 * 60 * 0.000621371)} mph</p>
+     </div>
+    `
+}
+// 60 * 60 * 0.000621371
+
+function callback(data){
+    STORE.data = data;  
+    console.log(data);
+    createHtml();  
+}
+
 function main(){
     handleFormSubmit();
     handleFormReSubmit(); 
 }
 
-function callback(data){
-    STORE.data = data;  
-    console.log(data); 
-}
+
 
 $(main)
 
