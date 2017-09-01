7e779c938a6501eed97562ce7bad41febcf72781
Anthony Mengel <anthonymengel1232@gmail.com>
2017-09-01 13:53:06 -0400

    finished switch statement

--- main.js
+++ main.js
@@ -93,40 +93,40 @@ function createDate(rawdata){
     let year = smallData[0]; 
     let month = smallData[1]; 
     switch(month){
-        case 01:
+        case '01':
             month = "January";
             break;
-        case 02:
+        case '02':
             month = "February";
             break;
-        case 03:
+        case'03':
             month = "March";
             break;
-        case 04:
+        case '04':
             month = "April";
             break;
-        case 05:
+        case '05':
             month = "May";
             break;
-        case 06:
+        case '06':
             month = "June";
             break;
-        case 07:
+        case '07':
             month = "July";
             break;
-        case 08:
+        case '08':
             month = "August";
             break;
         case "09":
             month = "September";
             break;
-        case 10:
+        case '10':
             month = "October";
             break;
-        case 11:
+        case '11':
             month = "November";
             break;
-        case 12:
+        case '12':
             month = "December";
             break;
         default: 
